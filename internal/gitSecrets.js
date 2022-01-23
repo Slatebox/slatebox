975561a2123fc5e677b965242a189a1d8204955f
Tim Heckel <tim@slatebox.com>
2022-01-23 11:39:38 -0600

    internal tools created

---
+++ internal/gitSecrets.js
@@ -0,0 +1,23 @@
+
+// const sodium = require('tweetsodium');
+// const key = "base64-encoded-public-key";
+// const value = "plain-text-secret";
+
+
+// // Convert the message and key to Uint8Array's (Buffer implements that interface)
+// const messageBytes = Buffer.from(value);
+// const keyBytes = Buffer.from(key, 'base64');
+
+
+// // Encrypt using LibSodium.
+// const encryptedBytes = sodium.seal(messageBytes, keyBytes);
+
+
+// // Base64 the encrypted secret
+// const encrypted = Buffer.from(encryptedBytes).toString('base64');
+
+// call setsbprodenv prior to running this
+
+for (let env of ["ROOT_URL", "MONGO_URL", "MONGO_OPLOG_URL", "PORT", "METEOR_SETTINGS"]) {
+    console.log(`${env}: ${Buffer.from(process.env[env]).toString("base64")}`);
+}
