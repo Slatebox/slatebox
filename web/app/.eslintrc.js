727ef5e623bda0e38be70b9d2a02e24becf02825
Tim Heckel <tim@slatebox.com>
2022-03-01 07:53:09 -0600

    eslint tweak for meteor

--- web/app/.eslintrc.js
+++ web/app/.eslintrc.js
@@ -13,5 +13,7 @@ module.exports = {
     sourceType: 'module',
   },
   plugins: ['react'],
-  rules: {},
+  rules: {
+    'import/no-unresolved': ['error', { ignore: ['^meteor/', '^/'] }],
+  },
 }
--- web/app/client/main.jsx
+++ web/app/client/main.jsx
@@ -1,11 +1,12 @@
+/* eslint-disable no-underscore-dangle */
 import React from 'react'
 import { Provider } from 'react-redux'
-import store from './store'
 import { Meteor } from 'meteor/meteor'
 import { render } from 'react-dom'
-import App from '../imports/ui/App.jsx'
 import has from 'lodash.has'
 import i18n from 'meteor/universe:i18n'
+import App from '../imports/ui/App'
+import store from './store'
 
 Meteor.startup(() => {
   // options
@@ -20,7 +21,7 @@ Meteor.startup(() => {
     document.getElementById('react-target')
   )
 
-  //swallow debug messages
+  // swallow debug messages
   Meteor._debug = (function (super_meteor_debug) {
     return function (error, info) {
       if (!(info && has(info, 'msg'))) {
